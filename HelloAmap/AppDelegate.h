840eb85f798bbebe03ec72efd5cf6685b6b5fc13
AbelSu <abel@abeldemacbook-pro.local>
2015-08-26 00:00:30 +0800

    Initial Commit

---
+++ HelloAmap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloAmap
+//
+//  Created by abel on 15/8/26.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloAmap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloAmap
+//
+//  Created by abel on 15/8/26.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
